200f598e7864fa5c5ad0b8f3cb57b33ccf4fe720
Chinni-Chinni <enjarappachinni2001@gmil.com>
2022-05-18 00:10:20 +0530

    popularmovies

--- throwDice/throwDIse.js
+++ throwDice/throwDIse.js
@@ -1,41 +1,53 @@
-//SELECTING RANDOM NUMBER FROM 1 TO 6
+var firstRandomNum;
+var secondRandomNum;
+var thirdRandomNum;
+document.querySelector('#throwDice1').addEventListener('click',function(){
+    event.preventDefault();
+    firstRandomNum = Math.floor(Math.random()*6)+1;
+    var firstDiceImage = 'dice' + firstRandomNum + '.png';
+    document.querySelectorAll('img')[0].setAttribute("src",firstDiceImage);
+// //console.log(firstDiceImage);
+    winners(firstRandomNum,secondRandomNum,thirdRandomNum);
+});
+//console.log(firstRandomNum);
 
-var firstRandomNum = Math.floor(Math.random()*6)+1;
-var secondRandomNum = Math.floor(Math.random()*6)+1;
-var thirdRandomNum = Math.floor(Math.random()*6)+1;
+document.querySelector('#throwDice2').addEventListener('click',function(){
+    event.preventDefault();
+    secondRandomNum = Math.floor(Math.random()*6)+1;
+    var secondDiceImage = 'dice' + secondRandomNum + '.png';
+    document.querySelectorAll('img')[1].setAttribute("src",secondDiceImage);
 
-//SETTING THE IMAGE
-var firstDiceImage = 'dises/dice' + firstRandomNum + '.png';
+    winners(firstRandomNum,secondRandomNum,thirdRandomNum);
 
-var secondDiceImage = 'dises/dice' + secondRandomNum + '.png';
+});
 
-var thirdDiceImage = 'dises/dice' + thirdRandomNum + '.png';
+document.querySelector('#throwDice3').addEventListener('click',function(){
+    event.preventDefault();
+    thirdRandomNum = Math.floor(Math.random()*6)+1;
+    var thirdDiceImage = 'dice' + thirdRandomNum + '.png';
+    document.querySelectorAll('img')[2].setAttribute("src",thirdDiceImage);
+    winners(firstRandomNum,secondRandomNum,thirdRandomNum);
+});
 
-//SETIING THE DICE TO THE IMAGE
-
-
-document.querySelectorAll('img')[0].setAttribute("src",firstDiceImage);
-
-document.querySelectorAll('img')[1].setAttribute("src",secondDiceImage);
-
-document.querySelectorAll('img')[2].setAttribute("src",thirdDiceImage);
 
 
 ///CHECKING WHO IS WINNER OUT OF THREE MEMBERS
-
-if(firstRandomNum>secondRandomNum && firstRandomNum>thirdRandomNum){
-    document.querySelector('h1').innerText = "Winner is User1";
-
-}else if(secondRandomNum>firstRandomNum && secondRandomNum > thirdRandomNum){
-    document.querySelector("h1").innerText = "Winner is User2"
+function winners(firstRandomNum,secondRandomNum,thirdRandomNum){
+    if(firstRandomNum>secondRandomNum && firstRandomNum>thirdRandomNum){
+        document.querySelector('h1').innerText = "Winner is User1";
+    
+    }else if(secondRandomNum>firstRandomNum && secondRandomNum > thirdRandomNum){
+        document.querySelector("h1").innerText = "Winner is User2"
+    }
+    else if(thirdRandomNum>firstRandomNum && thirdRandomNum>secondRandomNum){
+        document.querySelector("h1").innerText = "Winner is User3"
+     }else if(thirdRandomNum===firstRandomNum){
+        document.querySelector("h1").innerText="User1 and User3 are Winners";
+    }else if(firstRandomNum===secondRandomNum){
+        document.querySelector("h1").innerText = "User1 and User2 are Winners";
+    }
+    else if(secondRandomNum===thirdRandomNum){
+        document.querySelector("h1").innerText = "User2 and User3 are Winners";
+    }
 }
-else if(thirdRandomNum>firstRandomNum && thirdRandomNum>secondRandomNum){
-    document.querySelector("h1").innerText = "Winner is User3"
- }else if(thirdRandomNum===firstRandomNum){
-    document.querySelector("h1").innerText="User1 and User3 are Winners";
-}else if(firstRandomNum===secondRandomNum){
-    document.querySelector("h1").innerText = "User1 and User2 are Winners";
-}
-else if(secondRandomNum===thirdRandomNum){
-    document.querySelector("h1").innerText = "User2 and User3 are Winners";
-}
+// winners();
